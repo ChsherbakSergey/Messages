ce93f752a37c771799524d8dfb34626d75036488
chsherbak <chsherbaks@gmail.com>
2020-11-15 13:36:59 +0300

    Getting Started

---
+++ Messages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messages
+//
+//  Created by Sergey on 11/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
